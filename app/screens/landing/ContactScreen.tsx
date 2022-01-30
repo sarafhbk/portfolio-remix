b1affd20434b84c04fbc0237ae82f6cf3cb42b2c
sarafhbk <saraffath@gmail.com>
2022-01-30 21:02:38 +0530

    UI : text align center for mobile devices

--- app/screens/landing/ContactScreen.tsx
+++ app/screens/landing/ContactScreen.tsx
@@ -1,7 +1,7 @@
 export default function ContactScreen() {
   return (
     <div className="p-4 h-full flex items-center justify-center">
-      <h1 className="text-2xl font-semibold px-4 max-w-2xl leading-relaxed md:text-4xl md:leading-loose">
+      <h1 className="text-2xl font-semibold px-4 max-w-2xl leading-relaxed text-center md:text-4xl md:leading-loose lg:text-left">
         You can reach me for an inquiry or just to say hi at
         saraffath@gmail.com.
       </h1>
--- app/screens/landing/LandingScreen.tsx
+++ app/screens/landing/LandingScreen.tsx
@@ -1,7 +1,7 @@
 export default function LandingScreen() {
   return (
     <div className="p-4 h-full flex items-center justify-center">
-      <h1 className="text-2xl font-semibold px-4 max-w-2xl leading-relaxed md:text-4xl md:leading-loose">
+      <h1 className="text-2xl font-semibold px-4 max-w-2xl leading-relaxed text-center md:text-4xl md:leading-loose lg:text-left">
         hi, i am saraf, a front-end developer based in india. i strive to make
         the web a beautiful place.
       </h1>
